30285060384e2c964d79e5f7028707e1cf83bffb
sml <smlrodrigues.dev@gmail.com>
2023-04-27 13:43:18 -0300

    Add authentication routes

---
+++ routes/authentication.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+// Require controller modules
+const signup_controller = require('../models/signupController');
+const login_controller = require('../models/loginController');
+
+// SIGNUP ROUTES //
+
+// GET request for creating a User.
+router.get('/sign-up', signup_controller.user_create_get);
+
+// POST request for creating a User.
+router.post('/sign-up', signup_controller.user_create_post);
+
+// LOGIN ROUTES //
+
+// GET request for reading a User
+router.get('/log-in', login_controller.user_read_get);
+
+// POST request for reading a User
+router.post('/log-in', login_controller.user_read_post);
+
+module.exports = router;
