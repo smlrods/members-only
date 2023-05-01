e2a1fe0295efbdb4f3e35242fe1dc1f5d1fe5f7d
sml <smlrodrigues.dev@gmail.com>
2023-05-01 08:22:01 -0300

    Display all member messages on the home page

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Message = require('../models/message');
 
 router.get('/', function(req, res, next) {
   res.redirect('/home');
 });
 
 /* GET home. */
-router.get('/home', function(req, res, next) {
-  res.render('home', {user: req.user});
+router.get('/home', async (req, res, next) => {
+  const messages = await Message.find().exec();
+  res.render('home', {
+    user: req.user,
+    messages: messages
+  });
 });
 
 module.exports = router;
